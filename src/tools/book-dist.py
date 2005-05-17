afa8072dbd1c216340c9d7436be91bddd05cd004
cmpilato <cmpilato@931749d0-5854-0410-9456-f14be4d6b398>
2005-05-17 23:10:37 +0000

    Rework book-dist.py to be a little more flexible (and fix a tarball generation bug).
    git-svn-id: http://svnbook.googlecode.com/svn/trunk@1306 931749d0-5854-0410-9456-f14be4d6b398

--- src/tools/book-dist.py
+++ src/tools/book-dist.py
@@ -3,44 +3,86 @@
 import sys
 import os
 import shutil
+import getopt
+
+
 
 def die(msg):
-  sys.stderr.write('ERROR: ' + msg)
-  sys.exit(1)
+    sys.stderr.write('ERROR: ' + msg)
+    sys.exit(1)
 
-if len(sys.argv) < 2:
-  die("Usage: %s LOCALE [TARGET...]\n" % (os.path.basename(sys.argv[0])))
+
+def usage(errorcode):
+    stream = errorcode and sys.stderr or sys.stdout
+    stream.write("""Usage: %s OPTIONS
+
+Options:
+   --html:         Make the single-page HTML book
+   --html-chunk:   Make the chunked HTML book
+   --pdf:          Make the PDF book
+   --name:         The base name of the tarball, and top-level tar directory
+""" % (os.path.basename(sys.argv[0])))
+    sys.exit(errorcode)
+    
+
+def main():
+    try:
+        optlist, args = getopt.getopt(sys.argv[1:], "h",
+                                      ['help', 'html', 'html-chunk',
+                                       'pdf', 'name='])
+    except:
+        usage(1)
+    html = html_chunk = pdf = 0
+    name = 'svnbook'
+    targets = []
+    for opt, arg in optlist:
+        if opt == '--help' or opt == '-h':
+            usage(0)
+        if opt == '--html':
+            html = 1
+        if opt == '--html-chunk':
+            html_chunk = 1
+        if opt == '--pdf':
+            pdf = 1
+        if opt == '--name':
+            name = arg
+
+    if pdf and not os.getenv('JAVA_HOME'):
+        die('JAVA_HOME is not set.\n')
+
+    if os.path.basename(name) != name:
+        die('Name "%s" is not a single path component\n' % (name))
+        
+    if html: targets.append('install-book-html')
+    if html_chunk: targets.append('install-book-html-chunk')
+    if pdf: targets.append('install-book-pdf')
+
+    if len(targets) < 1:
+        die('No targets specified.\n')
+        
+    if not os.path.exists('book') or not os.path.exists('Makefile'):
+        die('Please run this from the Subversion book source directory.\n')
   
-elif len(sys.argv) == 2:
-  locale = sys.argv[1]
-  targets = ['install-book-html',
-             'install-book-html-chunk',
-             'install-book-pdf']
-else:
-  locale = sys.argv[1]
-  targets = sys.argv[2:]
+    os.putenv('FOP_OPTS', '-Xms100m -Xmx200m')
 
-cwd = os.getcwd()
-if not os.path.exists('book') \
-   or not os.path.exists('Makefile'):
-  die('Please run this from the Subversion book source directory\n')
+    def _cleanup_tmp_dirs():
+        if os.path.exists(name): shutil.rmtree(name)
+        if os.path.exists('__SVNBOOK_TMP__'): shutil.rmtree('__SVNBOOK_TMP__')
+      
+    try:
+        _cleanup_tmp_dirs()
+        os.mkdir('__SVNBOOK_TMP__')
+        os.system('DESTDIR=__SVNBOOK_TMP__ make book-clean %s' \
+                  % (' '.join(targets)))
+        os.rename('__SVNBOOK_TMP__/usr/share/doc/subversion/book', name)
+        os.system('tar cvfz %s.tar.gz %s' % (name, name))
+    finally:
+        _cleanup_tmp_dirs()
   
-if not os.getenv('JAVA_HOME'):
-  die('JAVA_HOME is not set.\n')
+    if not os.path.exists(name + '.tar.gz'):
+        die('Hrm.  It appears the tarball was not created.\n')
 
-tarball_base = os.path.join(cwd, 'svnbook-%s' % (locale))
-os.putenv('FOP_OPTS', '-Xms100m -Xmx200m')
+    print 'Tarball %s.tar.gz created.  Enjoy!' % (name)
 
-try:
-  os.mkdir('svnbook-tmp')
-  os.system('DESTDIR=svnbook-tmp make book-clean %s' % (' '.join(targets)))
-  os.rename('svnbook-tmp/usr/share/doc/subversion/book', tarball_base)
-  os.system('tar cvfz ' + tarball_base + '.tar.gz ' + tarball_base)
-finally:
-  shutil.rmtree(tarball_base)
-  shutil.rmtree('svnbook-tmp')
-  
-if not os.path.exists(tarball_base + '.tar.gz'):
-  die('Hrm.  It appears the tarball was not created.\n')
-
-print 'Your tarball sits in %s.tar.gz.  Enjoy!' % (tarball_base)
+if __name__ == "__main__":
+    main()
