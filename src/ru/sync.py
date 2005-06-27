45632f92bef4516676a10e5f6922d25796b28719
dmitriy <dmitriy@931749d0-5854-0410-9456-f14be4d6b398>
2005-06-27 22:31:35 +0000

    * ru/TRANSLATION-STATUS: Having progress for ch03.xml, 50% -> 75%. * ru/sync.py: Completely rewritten. * ru/book/ch03.xml: 25% of tranlation are added.
    git-svn-id: http://svnbook.googlecode.com/svn/trunk@1501 931749d0-5854-0410-9456-f14be4d6b398

--- src/ru/sync.py
+++ src/ru/sync.py
@@ -1,101 +1,48 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 
 import sys, os, getopt, string
 
 def usage(err_msg):
-    stream = err_msg and sys.stderr or sys.stdout
-    if err_msg:
-        stream.write("ERROR: %s\n\n" % (err_msg))
-    stream.write("""Usage: %(name)s OPTIONS
+  stream = err_msg and sys.stderr or sys.stdout
+  if err_msg:
+    stream.write("ERROR: %s\n\n" % (err_msg))
+  stream.write("""Usage: %(name)s <subcommand> [filename]
 
-Options:
-   -f:     <filename.xml>
-   -a:     <action>, can be [log | diff | merge]
-   -b:     Merging ru/ with en/
-   -l:     View log for en/book/
+Valid subcommands:
+    diff (di)
+    log
+    merge
 
 Examples:
-   %(name)s -f book/ch01.xml -a diff   <- view diff for ch01.xml
-   %(name)s -b                         <- merging ru/ with en/
-   %(name)s -l                         <- view log for en/book/
+   %(name)s log book/foreword.xml
+   %(name)s diff
 """ % { 'name' : os.path.basename(sys.argv[0]) })
-    sys.exit(err_msg and 1 or 0)
+  sys.exit(err_msg and 1 or 0)
 
 def main():
 
-    if len(sys.argv) < 2:
-      usage(None)
+  if len(sys.argv) < 2:
+    usage(None)
 
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "f:a:bl")
-    except:
-        usage("Invalid syntax")
+  if not sys.argv[1] in ("diff", "di", "log", "merge"):
+    usage("Invalid syntax")
 
-    fname = action = ''
-    bmerge = blog = False
+  book_src_url = "https://svn.red-bean.com/svnbook/trunk/src/en/book/"
+  fd = open("LAST_UPDATED", "r")
+  last_up_rev = fd.readline()
+  fd.close()
 
-    book_src = os.listdir("book")
-    file_actions = ("log", "diff", "merge")
-    book_src_url = "https://svn.red-bean.com/svnbook/trunk/src/en/"
+  print "svn update"
+  os.system("svn update")
+  os.chdir("book")
 
-    for o, a in opts:
-        if o == "-f":
-            if os.path.basename(a) in book_src:
-                fname = a
-        elif o == "-a":
-            if a in file_actions:
-                action = a
-        elif o == "-b":
-            bmerge = True
-        elif o == "-l":
-            blog = True
-
-    fd = open("LAST_UPDATED", "r")
-    last_up_rev = fd.readline()
-    fd.close()
-
-    attention = """ATTENTION! First it is necessary to make synchronization
-for separate files (and commit performed changes if during
-synchronization you had conflicts)!
-Continue? [y/N] """
-
-    merge_error = """During merge there were some errors. Solve a problem
-and start merge once again."""
-
-    if bmerge:
-        if string.lower(raw_input(attention)) == 'y':
-            print "svn update"
-            os.system("svn update")
-            os.chdir("book")
-            cmd = "svn merge -r " + last_up_rev + ":HEAD " + \
-                  book_src_url + "book/"
-            print cmd
-            os.system(cmd)
-            os.chdir("..")
-            os.system("svnversion . --no-newline > LAST_UPDATED")
-            fd = open("LAST_UPDATED", "r")
-            s = fd.readline()
-            fd.close()
-            if s != last_up_rev:
-                print merge_error
-                os.system("svn revert LAST_UPDATED")
-        sys.exit()
-
-    if blog:
-        cmd = "svn log -r " + last_up_rev + ":HEAD " + book_src_url + "book/"
-        print cmd
-        os.system(cmd)
-        sys.exit()
-
-    if fname == '' or action == '':
-        usage("Invalid syntax")
-
-    print "svn update"
-    os.system("svn update")
-
-    os.chdir("book")
-    cmd = "svn " + action + " -r " + last_up_rev + ":" + "HEAD" + " " + \
-          book_src_url + fname
+  if len(sys.argv) > 2:
+    cmd = "svn " + action + " -r " + last_up_rev + ":HEAD " + book_src_url + \
+          os.path.basename(sys.argv[2])
+    print cmd
+    os.system(cmd)
+  else:
+    cmd = "svn " + action + " -r " + last_up_rev + ":HEAD " + book_src_url
     print cmd
     os.system(cmd)
 
