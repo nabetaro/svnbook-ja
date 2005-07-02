febb48ead1f6a781450b4ac458fc1e26f88378bf
dmitriy <dmitriy@931749d0-5854-0410-9456-f14be4d6b398>
2005-07-02 12:00:35 +0000

    * ru/TRANSLATION-STATUS   Status of files ch03.xml, ch04.xml is changed
    * ru/statusof.py
      New script for a rough estimate of quantity of the translated text
    
    * ru/sync.py
      Fixing bug with 'action'
    
    * ru/book/ch00.xml
      Replacing <TAB> with <SPACE>
    
    * ru/book/ch03.xml
      Translation finished
    
    * ru/book/ch04.xml
      Translation started
    
    
    git-svn-id: http://svnbook.googlecode.com/svn/trunk@1511 931749d0-5854-0410-9456-f14be4d6b398

--- src/ru/sync.py
+++ src/ru/sync.py
@@ -2,21 +2,20 @@
 
 import sys, os, string
 
+subcmd = ("diff", "di", "log", "merge")
+
 def usage(err_msg):
   stream = err_msg and sys.stderr or sys.stdout
   if err_msg:
     stream.write("ERROR: %s\n\n" % (err_msg))
   stream.write("""Usage: %(name)s <subcommand> [filename]
 
-Valid subcommands:
-    diff (di)
-    log
-    merge
+Valid subcommands: %(valid_subcmd)s
 
 Examples:
    %(name)s log book/foreword.xml
    %(name)s diff
-""" % { 'name' : os.path.basename(sys.argv[0]) })
+""" % { 'name' : os.path.basename(sys.argv[0]), 'valid_subcmd' : subcmd })
   sys.exit(err_msg and 1 or 0)
 
 def main():
@@ -24,7 +23,7 @@ def main():
   if len(sys.argv) < 2:
     usage(None)
 
-  if not sys.argv[1] in ("diff", "di", "log", "merge"):
+  if not sys.argv[1] in subcmd:
     usage("Invalid syntax")
 
   book_src_url = "https://svn.red-bean.com/svnbook/trunk/src/en/book/"
@@ -37,19 +36,20 @@ def main():
   os.chdir("book")
 
   if len(sys.argv) > 2:
-    cmd = "svn " + action + " -r " + last_up_rev + ":HEAD " + book_src_url + \
+    cmd = "svn " + sys.argv[1] + " -r " + last_up_rev + ":HEAD " + book_src_url + \
           os.path.basename(sys.argv[2])
     print cmd
     os.system(cmd)
   else:
-    cmd = "svn " + action + " -r " + last_up_rev + ":HEAD " + book_src_url
+    cmd = "svn " + sys.argv[1] + " -r " + last_up_rev + ":HEAD " + book_src_url
     print cmd
     os.system(cmd)
-    if string.lower(raw_input("Whether to udate 'LAST_UPDATED'? [y/N] ")) == 'y':
-      os.chdir("..")
-      cmd = "svnversion . --no-newline > LAST_UPDATED"
-      print cmd
-      os.system(cmd)
+    if sys.argv[1] == 'merge':
+      if string.lower(raw_input("Whether to udate 'LAST_UPDATED'? [y/N] ")) == 'y':
+        os.chdir("..")
+        cmd = "svnversion . --no-newline > LAST_UPDATED"
+        print cmd
+        os.system(cmd)
 
 if __name__ == "__main__":
     main()
