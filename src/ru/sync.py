2d15c1b3a8412aa71e1f2050a9363463345ca296
dmitriy <dmitriy@931749d0-5854-0410-9456-f14be4d6b398>
2006-09-03 14:24:24 +0000

    * ru/sync.py: New function for the automatical updating ...   ru/TRANSLATION-STATUS: ... this file
    git-svn-id: http://svnbook.googlecode.com/svn/trunk@2402 931749d0-5854-0410-9456-f14be4d6b398

--- src/ru/sync.py
+++ src/ru/sync.py
@@ -15,8 +15,9 @@ def usage(err_msg):
 
 Options:
     -a:  Synchronizing all files
-    -f:  File which needs to be synchronized
+    -f:  File(s) which needs to be synchronized
     -l:  List all files and revisions with which they are sinchronized
+    -u:  Update TRANSLATION-STATUS file
     
     --dry-run:  Execute a command, but don't make actual changes
 """ % (os.path.basename(sys.argv[0])))
@@ -46,7 +47,7 @@ def get_base(fname = ''):
       rlist.append(rev)
     return max(rlist)
 
-def get_status(fname):
+def get_percent(fname):
   f = file(fname)
   ru = 0.0
   en = 0.001
@@ -65,7 +66,7 @@ def get_status(fname):
       in_para = False
     if re.search('@ *ENGLISH \}\}\} -->$', line):
       in_tr = False
-  return "(%3.2f%%)" % ((ru / en) * 100)
+  return '%i%%' % ((ru / en) * 100)
 
 def get_list():
   import platform
@@ -82,14 +83,55 @@ def get_list():
   rdelta2 = rbase - ((rbase - rmin) / 3)
   for f, r in frlist:
     if platform.system() == 'Windows':
-      print f, '\t', r, '\t', get_status(f)
+      sys.stdout.write(f+'\t'+str(r)+'\t'+get_percent(f)+'\t'+get_status(f))
     else:
       if r in range(rdelta2, rbase):
-        print '\x1b[32m', f, '\t', r, '\t', get_status(f), '\x1b[0m'
+        sys.stdout.write('\x1b[32m'+f+'\t'+str(r)+'\t'+get_percent(f)+
+            '\t'+get_status(f)+'\x1b[0m')
       elif r in range(rdelta1, rdelta2):
-        print '\x1b[33m', f, '\t', r, '\t', get_status(f), '\x1b[0m'
+        sys.stdout.write('\x1b[33m'+f+'\t'+str(r)+'\t'+get_percent(f)+
+            '\t'+get_status(f)+'\x1b[0m')
       elif r in range(rmin, rdelta1):
-        print '\x1b[31m', f, '\t', r, '\t', get_status(f), '\x1b[0m'
+        sys.stdout.write('\x1b[31m'+f+'\t'+str(r)+'\t'+get_percent(f)+
+            '\t'+get_status(f)+'\x1b[0m')
+  print
+
+def get_status(fname):
+  return os.popen('svn propget status '+fname).readline()
+
+def update_status_file():
+  global bfiles
+  st_f = file('../TRANSLATION-STATUS')
+  tmp_st_f = file('../TRANSLATION-STATUS.tmp', 'w')
+  curr_sta = ''
+  in_sta = False
+  indention = '        '
+  for line in st_f:
+    if in_sta:
+      if re.match(' {8}\*', line):
+        fcount = 0
+        for f in bfiles:
+          if re.match(curr_sta, get_status(f)):
+            if curr_sta == 'Transl':
+              tmp_st_f.write(indention+'* '+f+', '+get_percent(f)+'\n')
+            else:
+              tmp_st_f.write(indention+'* '+f+'\n')
+            fcount += 1
+        if fcount == 0:
+          tmp_st_f.write(indention+'* [none]\n')
+        in_sta = False
+      else:
+        tmp_st_f.write(line)
+    else:
+      if re.match(' {2}\w', line):
+        in_sta = True
+        curr_sta = line[2:8]
+      if not re.match(' {8}\*', line):
+        tmp_st_f.write(line)
+  tmp_st_f.close()
+  st_f.close()
+  os.remove('../TRANSLATION-STATUS')
+  os.rename('../TRANSLATION-STATUS.tmp', '../TRANSLATION-STATUS')
 
 def main():
   global bfiles
@@ -100,7 +142,7 @@ def main():
   except:
     pass
   try:
-    opts, args = getopt.getopt(sys.argv[1:], 'af:l', ['dry-run'])
+    opts, args = getopt.getopt(sys.argv[1:], 'af:lu', ['dry-run'])
   except:
     usage('Invalid syntax')
   sync_list = []
@@ -119,6 +161,8 @@ def main():
       return get_list()
     elif o == '--dry-run':
       dry_run = True
+    elif o == '-u':
+      update_status_file()
   cmd = string.Template('svn $a -r $r1:$r2 \
       http://svn.red-bean.com/svnbook/trunk/src/en/book/$t')
   for fname in sync_list:
