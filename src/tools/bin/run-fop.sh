a1bd395853f294acbc3c1d3bbacacb248446dd9e
maxb <maxb@931749d0-5854-0410-9456-f14be4d6b398>
2006-11-19 19:53:37 +0000

    Remove grepping-out of [ERROR] lines from FOP output. Masking errors seems like a bad idea to me, and the quantity of errors is minimal compared to the rest of the noise that FOP produces.
    * src/tools/bin/run-fop.sh
    
    
    git-svn-id: http://svnbook.googlecode.com/svn/trunk@2539 931749d0-5854-0410-9456-f14be4d6b398

--- src/tools/bin/run-fop.sh
+++ src/tools/bin/run-fop.sh
@@ -44,7 +44,4 @@ fi
 
 echo "(Using '${FOP_PGM}' for FOP)"
 
-# FOP is noisy on stdout, and -q doesn't seem to help, so stuff that
-# garbage into /dev/null.
-${FOP_PGM} $@ | grep -v "\[ERROR\]"
-
+${FOP_PGM} "$@"
