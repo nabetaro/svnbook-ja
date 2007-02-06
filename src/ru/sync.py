3e243383f91b2cf5ac59b7f3b94f65c8681b960f
dmitriy <dmitriy@931749d0-5854-0410-9456-f14be4d6b398>
2007-02-06 10:32:44 +0000

    * src/ru/book/*   Remove property 'last-sync', and set this property on 'book' directory,   globally for all files.
    * src/ru/sync.py: move from per-file syncing to syncing all-files-at-once.
    
    * src/ru/TRANSLATION-STATUS: update status of translation.
    
    git-svn-id: http://svnbook.googlecode.com/svn/trunk@2662 931749d0-5854-0410-9456-f14be4d6b398

--- src/ru/sync.py
+++ src/ru/sync.py
@@ -1,51 +1,28 @@
 #!/usr/bin/env python
 
-import sys, os, string, getopt, re
-
-bfiles = ['appa.xml', 'appb.xml', 'appc.xml', 'book.xml', 'ch00.xml', \
-          'ch01.xml', 'ch02.xml', 'ch03.xml', 'ch04.xml', 'ch05.xml', \
-          'ch06.xml', 'ch07.xml', 'ch08.xml', 'ch09.xml', 'copyright.xml', \
-          'foreword.xml', 'styles.css']
+import sys, os, string, getopt, re, glob
 
 def usage(err_msg):
   stream = err_msg and sys.stderr or sys.stdout
   if err_msg:
     stream.write("ERROR: %s\n\n" % (err_msg))
-  stream.write("""Usage: %s [-a] [-l] [-f <filename(s)>] [--dry-run]
+  stream.write("""Usage: %s [-r <revision>] [-u] [--dry-run]
 
 Options:
-    -a:  Synchronizing all files
-    -f:  File(s) which needs to be synchronized
-    -l:  List all files and revisions with which they are sinchronized
-    -u:  Update TRANSLATION-STATUS file
-    
+    -r: Revision to sync
+    -u: Update TRANSLATION-STATUS file
+
     --dry-run:  Execute a command, but don't make actual changes
 """ % (os.path.basename(sys.argv[0])))
   sys.exit(err_msg and 1 or 0)
 
-def get_last(fname):
-  return int(os.popen('svn propget last-sync '+fname).readline())
+def get_last():
+  return int(os.popen('svn propget last-sync book').readline())
 
-def set_last(fname, last):
-  os.system('svn propset last-sync '+last+' '+fname)
-
-def get_base(fname = ''):
-  global bfiles
-  if fname != '':
-    for line in os.popen('svn info '+fname):
-      if re.match('Revision: ', line):
-        return int(re.sub('Revision: ', '', line))
-  else:
-    rmin = sys.maxint
-    rlist = []
-    for file in bfiles:
-      for line in os.popen('svn info '+file):
-        if re.match('Revision: ', line):
-          rev = int(re.sub('Revision: ', '', line))
-      if rev < rmin:
-        rmin = rev
-      rlist.append(rev)
-    return max(rlist)
+def set_last(last):
+  c = 'svn propset last-sync '+last+' book'
+  print c
+  os.system(c)
 
 def get_percent(fname):
   f = file(fname)
@@ -68,41 +45,13 @@ def get_percent(fname):
       in_tr = False
   return '%i%%' % ((ru / en) * 100)
 
-def get_list():
-  import platform
-  global bfiles
-  frlist = ()
-  rmin = sys.maxint
-  rbase = get_base() + 1
-  for file in bfiles:
-    rev = get_last(file)
-    if rev < rmin:
-      rmin = rev
-    frlist += (file, rev),
-  rdelta1 = rmin + ((rbase - rmin) / 3)
-  rdelta2 = rbase - ((rbase - rmin) / 3)
-  for f, r in frlist:
-    if platform.system() == 'Windows':
-      sys.stdout.write(f+'\t'+str(r)+'\t'+get_percent(f)+'\t'+get_status(f))
-    else:
-      if r in range(rdelta2, rbase):
-        sys.stdout.write('\x1b[32m'+f+'\t'+str(r)+'\t'+get_percent(f)+
-            '\t'+get_status(f)+'\x1b[0m')
-      elif r in range(rdelta1, rdelta2):
-        sys.stdout.write('\x1b[33m'+f+'\t'+str(r)+'\t'+get_percent(f)+
-            '\t'+get_status(f)+'\x1b[0m')
-      elif r in range(rmin, rdelta1):
-        sys.stdout.write('\x1b[31m'+f+'\t'+str(r)+'\t'+get_percent(f)+
-            '\t'+get_status(f)+'\x1b[0m')
-  print
-
 def get_status(fname):
   return os.popen('svn propget status '+fname).readline()
 
 def update_status_file():
-  global bfiles
-  st_f = file('../TRANSLATION-STATUS')
-  tmp_st_f = file('../TRANSLATION-STATUS.tmp', 'w')
+  bfiles = glob.glob('book/*.xml')
+  st_f = file('TRANSLATION-STATUS')
+  tmp_st_f = file('TRANSLATION-STATUS.tmp', 'w')
   curr_sta = ''
   in_sta = False
   indention = '        '
@@ -113,9 +62,9 @@ def update_status_file():
         for f in bfiles:
           if re.match(curr_sta, get_status(f)):
             if curr_sta == 'Transl':
-              tmp_st_f.write(indention+'* '+f+', '+get_percent(f)+'\n')
+              tmp_st_f.write(indention+'* '+os.path.basename(f)+', '+get_percent(f)+'\n')
             else:
-              tmp_st_f.write(indention+'* '+f+'\n')
+              tmp_st_f.write(indention+'* '+os.path.basename(f)+'\n')
             fcount += 1
         if fcount == 0:
           tmp_st_f.write(indention+'* [none]\n')
@@ -130,56 +79,39 @@ def update_status_file():
         tmp_st_f.write(line)
   tmp_st_f.close()
   st_f.close()
-  os.remove('../TRANSLATION-STATUS')
-  os.rename('../TRANSLATION-STATUS.tmp', '../TRANSLATION-STATUS')
+  os.remove('TRANSLATION-STATUS')
+  os.rename('TRANSLATION-STATUS.tmp', 'TRANSLATION-STATUS')
 
 def main():
-  global bfiles
   if len(sys.argv) < 2:
     usage(None)
   try:
-    os.chdir('book')
-  except:
-    pass
-  try:
-    opts, args = getopt.getopt(sys.argv[1:], 'af:lu', ['dry-run'])
+    opts, args = getopt.getopt(sys.argv[1:], 'r:u', ['dry-run'])
   except:
     usage('Invalid syntax')
-  sync_list = []
   dry_run = False
+  last = base = get_last()
   for o, a in opts:
-    if o == '-a':
-      sync_list = bfiles
-    elif o == '-f':
-      for f in a.split(','):
-        f = os.path.basename(f)
-        if f in bfiles:
-          sync_list.append(f)
-        else:
-          usage('Invalid syntax')
-    elif o == '-l':
-      return get_list()
+    if o == '-r':
+      base = int(a)
     elif o == '--dry-run':
       dry_run = True
     elif o == '-u':
-      update_status_file()
-  cmd = string.Template('svn $a -r $r1:$r2 \
-      http://svn.red-bean.com/svnbook/trunk/src/en/book/$t')
-  for fname in sync_list:
-    last = get_last(fname)
-    base = get_base(fname)
-    print ('########################################################################')
-    print 'Sync r%s:r%s of %s' % (last, base, fname)
-    diff = os.popen(cmd.substitute(a='diff', r1=last, r2=base, t=fname)).read()
-    if len(diff) != 0:
-      os.system(cmd.substitute(a='log', r1=last, r2=base, t=fname))
-      f = file('../'+fname+'.diff', 'w'); f.write(diff); f.close()
-      c = cmd.substitute(a='merge', r1=last, r2=base, t=fname)
-      if dry_run:
-        c += ' --dry-run'
-      os.system(c)
-    if not dry_run:
-      set_last(fname, str(base))
+      return update_status_file()
+  cmd = string.Template(
+      'svn $a -r $r1:$r2 http://svn.red-bean.com/svnbook/trunk/src/en/book/$t')
+  print ('########################################################################')
+  print 'Sync r%s:r%s' % (last, base)
+  diff = os.popen(cmd.substitute(a='diff', r1=last, r2=base, t='')).read()
+  if len(diff) != 0:
+    os.system(cmd.substitute(a='log', r1=last+1, r2=base, t=''))
+    c = cmd.substitute(a='merge', r1=last, r2=base, t=' book/')
+    if dry_run:
+      c += ' --dry-run'
+    print c
+    os.system(c)
+  if not dry_run:
+    set_last(str(base))
 
 if __name__ == "__main__":
   main()
