430280351a5d537d9c632beb128483a49d924646
cmpilato <cmpilato@931749d0-5854-0410-9456-f14be4d6b398>
2008-12-12 16:35:58 +0000

    * src/tools/bin/spew-svn-command-options.sh   Rename to...
    * src/tools/bin/spew-svn-command-options
      ...this, and rework this script to print a consistent,
      alphabetically sorted list of long options supported by various bits
      of the Subversion toolchain.  The goal, of course, being output that
      is 'diff'able from version to version.
    
    
    git-svn-id: http://svnbook.googlecode.com/svn/trunk@3373 931749d0-5854-0410-9456-f14be4d6b398

--- src/tools/bin/spew-svn-command-options.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-### Print 'svn' subcommands and their non-global, non-deprecated options. ###
-
-for subcommand in `svn help | grep '^   [a-z]' | cut -f 4 -d ' '`; do 
-    echo "*** ${subcommand} ***"
-    svn help ${subcommand} | cut -c3- | grep '^\-' | sort | grep -v '^-\(-username\|-password\|-non-interactive\|-no-auth-cache\|-config-dir\|N \)' | sed 's,^,   ,'
-done
